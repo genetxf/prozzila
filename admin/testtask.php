1cca45341b8da907d2889dc229e95aa7da45b19c
Syed Foyez Uddin <foyezashik1013105@gmail.com>
2024-01-17 07:00:18 +0600

    Icons online to offline accessible
    All online icons add offline local access.
    
    Co-Authored-By: alaminmirza <156717127+alaminmirza@users.noreply.github.com>

--- admin/testtask.php
+++ admin/testtask.php
@@ -18,7 +18,7 @@ include 'database.php';
     <link href="https://fonts.googleapis.com/css2?family=Roboto:ital,wght@0,100;0,300;0,400;0,500;0,700;0,900;1,100;1,300;1,400;1,500;1,700;1,900&display=swap"
           rel="stylesheet">
     <!-- BOXICONS -->
-    <link href='https://unpkg.com/boxicons@2.0.7/css/boxicons.min.css' rel='stylesheet'>
+    <link href='css/boxicons.min.css' rel='stylesheet'>
     <link rel="stylesheet" href="css/icons.min.css">
 
     <!-- APP CSS -->
@@ -28,7 +28,7 @@ include 'database.php';
     <link rel="stylesheet" href="./css/responsive.css">
 </head>
 <?php
-$pagetitle = "Board";
+$pagetitle = "Task";
 include 'header.php';
 
 ?>
