85d1209bc24e61d55acccbdf5753edfa1dfce7ec
Syed Foyez Uddin <foyezashik1013105@gmail.com>
2024-01-20 12:07:51 +0600

    Cards SQL Update
    Co-Authored-By: Mirza Alamin Hossen <156717127+alaminmirza@users.noreply.github.com>

--- admin/prboard.php
+++ admin/prboard.php
@@ -4,14 +4,16 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
     // Retrieve other form data
     $cardTitle = $_POST['cardTitle'];
     $description = $_POST['description'];
+    $start = $_POST['start'];
     $dueDate = $_POST['dueDate'];
+    $status = $_POST['status'];
 
     if ($conn->connect_error) {
         die("Connection failed: " . $conn->connect_error);
     }
 
     // Assuming you have a table named 'cards' with columns 'title', 'due_date', 'tasks', and 'file_path'
-    $sql = "INSERT INTO `cards`(`title`, `due_date`, `description`) VALUES ('$cardTitle', '$dueDate', '$description')";
+    $sql = "INSERT INTO `cards`(`title`, `due_date`,`start`, `description`, `status`) VALUES ('$cardTitle', '$dueDate', '$start', '$description', '$status')";
 
     if ($conn->query($sql) === TRUE) {
         echo "Card added successfully";
